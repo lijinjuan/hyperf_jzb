2f08a8f211556ae7c5c3b3e03bd2562348dc42c5
lijinjuan <1186747596@qq.com>
2022-03-07 11:24:29 +0800

    测试

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'f8fee4784f126ac906b10674f1a0559a6683fcb9',
+    'name' => 'jzb/library',
+  ),
+  'versions' => 
+  array (
+    'jzb/library' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f8fee4784f126ac906b10674f1a0559a6683fcb9',
+    ),
+  ),
+);
